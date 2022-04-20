9746726d88047e2717ae3fe3416937d6bbb00f16
Kálley Wilkerson <kalleywra@gmail.com>
2022-04-19 22:38:37 -0300

    I finally found something, yeahgit status

--- action_server.py
+++ action_server.py
@@ -1,12 +1,16 @@
 from rclpy.node import Node
 from rclpy.action import ActionServer
-from rclpy.action.server import ServerGoalHandle
+from rclpy.action.server import ServerGoalHandle, GoalEvent
 from nav2_msgs.action import NavigateToPose
+from rclpy.callback_groups import ReentrantCallbackGroup
 
 
 import rclpy
 import time
 import math
+
+from logging import getLogger
+import logging
 
 class Robot(object):
 
@@ -49,63 +53,75 @@ class Ros2ActionServer(Node):
             self,
             action_name="nav_to_pose_test",
             action_type=NavigateToPose,
-            execute_callback=self.execute_callback
+            execute_callback=self.execute_callback,
+            handle_accepted_callback=self.handle_accepted_callback
         )
         self.current_goal = None
         self.robot = robot
         self.get_logger().info("Ros2 action server created.")
-
-    def execute_callback(self, goal_handle: ServerGoalHandle):
-        self.get_logger().info("Received a goal from a client")
-
-        done = False
-        result = NavigateToPose.Result()
+        self.goal_qeue = []
+    
+    def handle_accepted_callback(self, goal_handle: ServerGoalHandle):
+        self.goal_qeue.append(goal_handle)
         goal_pose = goal_handle.request.pose.pose
-        self.get_logger().info("Goal received:")
-        self.get_logger().info(str(goal_pose))
+        self.get_logger().info("Received a goal from a client: " + str(goal_pose))
+        self.get_logger().info("Setting goal...")
         self.current_goal = goal_handle
         self.robot.destiny = (goal_pose.position.x, goal_pose.position.y)
+        self.get_logger().info('Deferring execution...')
 
-        rate = self.create_rate(2.0) # this should be used only in sigle threaded
+    def execute_callback(self, goal_handle: ServerGoalHandle):
+        self.get_logger().info("Robot final position: %f, %f" % (self.robot.pos[0], self.robot.pos[1]))
+        result = NavigateToPose.Result()
+        return result
 
-        while not done and rclpy.ok():
-            self.get_logger().info("Processing...")
-            self.get_logger().info("Robot position: %f, %f" % (self.robot.pos[0], self.robot.pos[1]))
-            if self.robot.arrived():
-                self.get_logger().info("Yeah, the robot arrived.")
-                self.current_goal.succeed()
-                done = True
-            else:
-                self.get_logger().info("Moving robot towards destiny...")
+class RobotController(object):
+
+    def __init__(self, robot: Robot, goal_handle: ServerGoalHandle):
+        self.robot = robot
+        self.goal_handle = goal_handle
+
+    def move_towards_destiny(self):
+        if self.robot.destiny:
+            if self.robot.arrived() and self.goal_handle.is_active:
+                getLogger(self.__class__.__name__).info("Yeah, the robot arrived.")
+                self.goal_handle.execute(self.execute_callback)
+                return
+            getLogger(self.__class__.__name__).info("moving robot if necessary...")
+            self.robot.move_towards_destiny()
+            getLogger(self.__class__.__name__).info("Robot position: " + str(self.robot.pos[0]) + ", " + str(self.robot.pos[1]))
+            if self.goal_handle:
                 feedback = NavigateToPose.Feedback()
                 distance = self.robot.distance_remaining()
                 if distance:
                     feedback.distance_remaining = float(distance)
-                self.current_goal.publish_feedback(feedback)
-                self.robot.move_towards_destiny()
-            time.sleep(1.5)
+                self.goal_handle.publish_feedback(feedback)
+                getLogger(self.__class__.__name__).info("Published feedback...")
 
-            goal_handle.execute()
-
+    def execute_callback(self, goal_handle: ServerGoalHandle):
+        getLogger(self.__class__.__name__).info("Robot final position: %f, %f" % (self.robot.pos[0], self.robot.pos[1]))
+        if self.robot.arrived():
+            self.robot.destiny = None
+            goal_handle.succeed()
+        result = NavigateToPose.Result()
         return result
 
 def main():
+    logging.basicConfig()
+    logging.root.setLevel(logging.DEBUG)
     rclpy.init()
     robot = Robot()
     node = Ros2ActionServer(robot)
-
     goal = node.current_goal
+    controller = RobotController(robot, goal)
+
     i = 0
-    while i <= 150:
+    while i <= 50:
+        getLogger(__name__).info("Iteraction " + str(i))
         rclpy.spin_once(node, timeout_sec=2)
         goal = node.current_goal
-        if goal and not goal.is_active:
-            feedback = NavigateToPose.Feedback()
-            distance_remaining = robot.distance_remaining()
-            if distance_remaining:
-                feedback.distance_remaining = float(robot.distance_remaining())
-            goal.publish_feedback(feedback)
-            print('published feedback')
+        controller.goal_handle = goal
+        controller.move_towards_destiny()
         i += 1
         time.sleep(1)
     
